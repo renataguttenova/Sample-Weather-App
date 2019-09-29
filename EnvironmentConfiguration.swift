6c4698e80d4713bcdaf985e6210576541c5b9c33
Renata Guttenova <guttenova.renata@gmail.com>
2019-09-29 14:05:16 +0200

    Added .swiftlint.yml and .swiftformat files, added both pods into run phase of the build

--- EnvironmentConfiguration.swift
+++ EnvironmentConfiguration.swift
@@ -12,34 +12,34 @@ struct EnvironmentConfiguration: Decodable {
     private enum CodingKeys: String, CodingKey {
         case apiBaseUrl = "API_BASE_URL"
     }
-    
+
     static let current: EnvironmentConfiguration = {
         EnvironmentConfiguration.readConfiguration()
     }()
-    
+
     static let configurationFileName = "Info"
     static let configurationFileExtension = "plist"
-    
+
     let apiBaseUrl: String
-    
+
     static func readConfiguration() -> EnvironmentConfiguration {
         guard let url = Bundle.main.url(
             forResource: EnvironmentConfiguration.configurationFileName,
             withExtension: EnvironmentConfiguration.configurationFileExtension
-            ) else {
-                fatalError("Unable to construct the Environment configuration file URL.")
+        ) else {
+            fatalError("Unable to construct the Environment configuration file URL.")
         }
-        
+
         guard let data = try? Data(contentsOf: url) else {
             fatalError("Unable to read the Environment configuration file.")
         }
-        
+
         let decoder = PropertyListDecoder()
-        
+
         guard let configuration = try? decoder.decode(EnvironmentConfiguration.self, from: data) else {
             fatalError("Unable to decode the Environment configuration file.")
         }
-        
+
         return configuration
     }
 }
--- SampleWeatherAppTests/SampleWeatherAppTests.swift
+++ SampleWeatherAppTests/SampleWeatherAppTests.swift
@@ -6,11 +6,10 @@
 //  Copyright © 2019 Renata Guttenová. All rights reserved.
 //
 
-import XCTest
 @testable import SampleWeatherApp
+import XCTest
 
 class SampleWeatherAppTests: XCTestCase {
-
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
@@ -26,9 +25,8 @@ class SampleWeatherAppTests: XCTestCase {
 
     func testPerformanceExample() {
         // This is an example of a performance test case.
-        self.measure {
+        measure {
             // Put the code you want to measure the time of here.
         }
     }
-
 }
