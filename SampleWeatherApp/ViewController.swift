db9c66e9249ea1a85aa30cd2fa854510e2105a3f
Renata Guttenova <guttenova.renata@gmail.com>
2019-09-29 13:10:11 +0200

    Initial Commit

---
+++ SampleWeatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SampleWeatherApp
+//
+//  Created by Renata Guttenová on 29/09/2019.
+//  Copyright © 2019 Renata Guttenová. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
